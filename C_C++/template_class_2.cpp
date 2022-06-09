18a9f5581bf0c06f26a438a0da59c48a1c3f17c0
Sameer Khan <sameerkhanofficial9@gmail.com>
2022-06-09 15:08:14 +0530

    C++

---
+++ C_C++/template_class_2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Arithmetic
+{
+    private:
+    int a;
+    int b;
+
+    public:
+
+    Arithmetic(int a, int b)
+    {
+        this->a=a;
+        this->b=b;
+    }
+
+    int add()
+    {
+        int c;
+        c = a +b;
+        return c;
+    }
+
+    int sub()
+    {
+        int c;
+        c=a-b;
+        return c;
+    }
+
+};
+
+int main()
+{
+
+    Arithmetic ar(20,5);
+    cout<<"Addition : "<<ar.add()<<endl;
+    cout<<"Subtraction : "<<ar.sub()<<endl;
+    
+    return 0;
+
+}
